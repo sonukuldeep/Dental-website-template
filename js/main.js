bdcf378442ecf5f5b4721f291f94d5369b3eb35a
sonukuldeep <sonukuldeep@outlook.com>
2023-09-18 19:10:35 +0530

    done

--- js/main.js
+++ js/main.js
@@ -33,3 +33,60 @@ gsap.to('.nav-animation', {
         toggleClass: 'activate',
     }
 })
+/**
+ * Description:- pass class name to animate the element
+ * @param {string} className - Pass the class name to animate
+ * @returns {void}
+ */
+function scrollAnimation(className) {
+    const tl = gsap.timeline({
+        scrollTrigger: {
+            trigger: className,
+            start: "top 80%",
+            end: 'top center',
+            // markers: true
+        }
+    })
+
+    tl.fromTo(className, {
+        opacity: 0,
+        y: -80
+    }, {
+        opacity: 1,
+        y: 0,
+        stagger: 0.5
+    })
+}
+
+scrollAnimation('.scroll-trigger')
+scrollAnimation('.scroll-trigger-1')
+scrollAnimation('.scroll-trigger-2')
+scrollAnimation('.scroll-trigger-3')
+scrollAnimation('.scroll-trigger-4')
+scrollAnimation('.scroll-trigger-5')
+
+
+function animatateFrom(classname, x) {
+    const tl = gsap.timeline({
+        scrollTrigger: {
+            trigger: classname,
+            start: "top center",
+            end: "top center",
+            // markers: true
+        }
+    })
+
+    tl.fromTo(classname, {
+        // opacity: 0,
+        xPercent: x,
+    }, {
+        opacity: 1,
+        xPercent: 0,
+    })
+}
+
+animatateFrom('.scroll-trigger-6', -100)
+animatateFrom('.scroll-trigger-7', 100)
+scrollAnimation('.scroll-trigger-8')
+scrollAnimation('.scroll-trigger-9')
+scrollAnimation('.scroll-trigger-10')
